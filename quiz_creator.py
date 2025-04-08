d8acd0ca5dca4c4ac17b8954c1772fb62c62c829
Shane <palomoshanecraven@gmail.com>
2025-04-08 21:10:48 +0800

    Add if statements to direct the users to their choices and add except function to catch invalid input.

--- quiz_creator.py
+++ quiz_creator.py
@@ -1,18 +1,25 @@
 # Import libraries that will be needed.
 
-# Allow users to interact with the option.
 while True:
     try:
         print("Selection:")
         print("1. Add Category \n2. Access a category \n3. Exit \n")
 
+        # Allow users to interact with the option.
         choice = int(input("Enter a number of your choice: "))
     
+        # If users choose 1 allow them to add category.
+        if choice == 1:
+            pass
+        
+        # If users choose 2 allow them to access file and add questions.
+        if choice == 2:
+            pass
+        
+        # if users choose 3 they will be able to leave the program.
+        elif choice == 3:
+            break
+
+    #Catch Invalid input.
     except ValueError:
-        break
-
-# If users choose 1 allow them to add category.
-
-# If users choose 2 allow them to access file and add questions.
-
-# if users choose 3 they will be able to leave the program.
+        print("Invalid Input. Please, try again.")
