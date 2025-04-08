39262bd1c9c9a0e8528c4e7f6c37a9173b3eb88b
Shane <palomoshanecraven@gmail.com>
2025-04-08 21:02:24 +0800

    Main Pseudocode for this program.

---
+++ quiz_creator.py
@@ -0,0 +1,9 @@
+# Import libraries that will be needed.
+
+# Allow users to interact with the option.
+
+# If users choose 1 allow them to add category.
+
+# If users choose 2 allow them to access file and add questions.
+
+# if users choose 3 they will be able to leave the program.
