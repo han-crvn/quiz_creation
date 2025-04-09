1cbbe7736ad6faa1de0a32efda6072b5330e9254
Shane <palomoshanecraven@gmail.com>
2025-04-09 09:35:12 +0800

    Allow users to go back in main menu if they accidentally type 1 or 2.

--- quiz_creator.py
+++ quiz_creator.py
@@ -15,21 +15,27 @@ while True:
         # If users choose 1 allow them to add category.
         if choice == 1:
             
-            # Allow users to add the name of the category
-            category = input("\nEnter the name of the category: ")
+            while True:
 
-            # Format the category name to its file name.
-            file_name = "_".join(category.lower().split()) + ".txt"
+                # Allow users to add the name of the category
+                category = input("\nEnter the name of the category (0 to go back in main menu): ")
+                
+                # Add a break to allow users to go back in main menu.
+                if category == "0":
+                    break
 
-            # Create the txt file of category.
-            myFile = open(f"category_{file_name}", "w")
+                # Format the category name to its file name.
+                file_name = "_".join(category.lower().split()) + ".txt"
 
-            # In the first line of txt file, add the name of category.
-            myFile.write(f"===== {category.upper()} =====")
-            myFile.close()
+                # Create the txt file of category.
+                myFile = open(f"category_{file_name}", "w")
 
-            # Add a short description that the file is successfully added.
-            print(f"\n{category} is successfully saved as {file_name}.\n")
+                # In the first line of txt file, add the name of category.
+                myFile.write(f"===== {category.upper()} =====")
+                myFile.close()
+
+                # Add a short description that the file is successfully added.
+                print(f"\n{category} is successfully saved as {file_name}.\n")
 
         # If users choose 2 allow them to access file and add questions.
         elif choice == 2:
@@ -48,73 +54,79 @@ while True:
                 for num, file in enumerate(categories, 1):
                     print(f"{num}.) {file}")
 
-            try:
-                
-                # Allow users to choose category.
-                select_category = int(input("\nEnter the number of the chosen category: "))
+            while True:
 
-                # Access the category using index.
-                file_name = categories[select_category - 1]
+                try:
+                    
+                    # Allow users to choose category.
+                    select_category = int(input("\nEnter the number of the chosen category (0 to go back in main menu): "))
 
-                # Add note that the file is successfully chosen.
-                print(f"\n{file_name.title()} is successfully chosen.\n")
+                    # Add a break to allow users to go back in main menu.
+                    if select_category == 0:
+                        break
 
-                while True:
+                    # Access the category using index.
+                    file_name = categories[select_category - 1]
 
-                    # Allow users to create question.
-                    question = input("Enter the question: ")
+                    # Add note that the file is successfully chosen.
+                    print(f"\n{file_name.title()} is successfully chosen.\n")
 
-                    # Add the question in file.
-                    access_file = open(file_name, "a")
-                    access_file.write(f"\nQuestion: {question}\n")
-                    access_file.close()
+                    while True:
 
-                    # Allow users to add choices.
-                    for i in range(4):
-                        choice = input(f"Choice {chr(65 + i )}:")
+                        # Allow users to create question.
+                        question = input("Enter the question: ")
 
-                        # Add the choices in file.
+                        # Add the question in file.
                         access_file = open(file_name, "a")
-                        access_file.write(f"{chr(65 + i)}.) {choice}\n")
+                        access_file.write(f"\nQuestion: {question}\n")
                         access_file.close()
 
-                    # Allow users to input the correct answer.
-                    answer = input("Enter the correct letter(answer): ")
+                        # Allow users to add choices.
+                        for i in range(4):
+                            choice = input(f"Choice {chr(65 + i )}:")
 
-                    # Add the answer in file.
-                    if answer == "A" or answer == "B" or answer == "C" or answer == "D":
-                        access_file = open(file_name, "a")
-                        access_file.write(f"Answer: {answer}\n\n")
-                        access_file.close()
+                            # Add the choices in file.
+                            access_file = open(file_name, "a")
+                            access_file.write(f"{chr(65 + i)}.) {choice}\n")
+                            access_file.close()
 
-                    # Catch the invalid answer.
-                    else:
-                        print("Input a valid letter\n")
+                        # Allow users to input the correct answer.
+                        answer = input("Enter the correct letter(answer): ")
 
-                    # Inform the users that the message is successfully added.
-                    print("The question set is successfully added.\n")
+                        # Add the answer in file.
+                        if answer == "A" or answer == "B" or answer == "C" or answer == "D":
+                            access_file = open(file_name, "a")
+                            access_file.write(f"Answer: {answer}\n\n")
+                            access_file.close()
 
-                    # Ask users if they want to add more.
-                    try:
-                        ask_user = int(input("Do you want to input another set of question (1 = Yes, 2 = No): "))
-
-                        # Validate their decision.
-                        if ask_user == 1:
-                            continue
-
-                        elif ask_user == 2:
-                            break
-
+                        # Catch the invalid answer.
                         else:
-                            print("Invalid input! Please, try again.\n")
+                            print("Input a valid letter\n")
 
-                     # Catch Invalid input.
-                    except ValueError:
-                        print("\n Invalid input. Please, try again.\n")
+                        # Inform the users that the message is successfully added.
+                        print("The question set is successfully added.\n")
 
-            # Catch Invalid input.
-            except ValueError:
-                print("\nInvalid Input. Please, try again.\n")
+                        # Ask users if they want to add more.
+                        try:
+                            ask_user = int(input("Do you want to input another set of question (1 = Yes, 2 = No): "))
+
+                            # Validate their decision.
+                            if ask_user == 1:
+                                continue
+
+                            elif ask_user == 2:
+                                break
+
+                            else:
+                                print("Invalid input! Please, try again.\n")
+
+                        # Catch Invalid input.
+                        except ValueError:
+                            print("\n Invalid input. Please, try again.\n")
+
+                # Catch Invalid input.
+                except ValueError:
+                    print("\nInvalid Input. Please, try again.\n")
 
         # if users choose 3 they will be able to leave the program.
         elif choice == 3:
