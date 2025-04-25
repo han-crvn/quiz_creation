824ea85ed4a8f556524d78aadc45d9cccc772322
Shane <palomoshanecraven@gmail.com>
2025-04-25 13:52:13 +0800

    Main pseudocode for this program.

---
+++ quiz_player.py
@@ -0,0 +1,7 @@
+# Import libraries that will be needed.
+
+# Allow users to choose between the option.
+
+# If users choose option 1, allow users to choose and answer the quiz.
+
+# If users choose option 2, allow users to leave the program.
