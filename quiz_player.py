ecdcd5168e3d969c8b767bfa2bfa1493803e4028
Shane <palomoshanecraven@gmail.com>
2025-04-25 13:55:25 +0800

    Import libraries and add the needed variables.

--- quiz_player.py
+++ quiz_player.py
@@ -1,4 +1,10 @@
 # Import libraries that will be needed.
+import os
+import time
+
+# Add variables for score and total of quiz.
+score = 0
+total = 0
 
 # Allow users to choose between the option.
 
